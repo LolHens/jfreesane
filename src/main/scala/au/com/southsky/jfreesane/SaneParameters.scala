be1c7c065cecd4d11d876ad3a1d8f4f74c35c7b8
LolHens <pierrekisters@gmail.com>
2016-07-04 10:34:59 +0200

    ported SaneParameters

---
+++ src/main/scala/au/com/southsky/jfreesane/SaneParameters.scala
@@ -0,0 +1,31 @@
+package au.com.southsky.jfreesane
+
+import com.google.common.base.MoreObjects
+
+class SaneParameters private[jfreesane](val frame: Int, val lastFrame: Boolean, val bytesPerLine: Int, val pixelsPerLine: Int, var lineCount: Int, val depthPerPixel: Int) {
+  private val frameType: FrameType = SaneEnums.valueOf(classOf[FrameType], frame)
+
+  def getFrameType: FrameType = frameType
+
+  def isLastFrame: Boolean = lastFrame
+
+  def getBytesPerLine: Int = bytesPerLine
+
+  def getPixelsPerLine: Int = pixelsPerLine
+
+  def getLineCount: Int = lineCount
+
+  private[jfreesane] def setLineCount(lineCount: Int) = this.lineCount = lineCount
+
+  def getDepthPerPixel: Int = depthPerPixel
+
+  override def toString: String =
+    MoreObjects.toStringHelper(classOf[SaneParameters])
+      .add("frameType", frameType)
+      .add("isLastFrame", lastFrame)
+      .add("bytesPerLine", bytesPerLine)
+      .add("pixelsPerLine", pixelsPerLine)
+      .add("lineCount", lineCount)
+      .add("depthPerPixel", depthPerPixel)
+      .toString
+}
